868b11f3e39a79309ab2351d4e216a22fbdbceaa
Ravlon <ravlonyork@gmail.com>
2022-06-30 02:39:45 +0200

    New functions, Rewritten Function, Added Comments, Better Structure

--- functools.py
+++ functools.py
@@ -1,7 +1,5 @@
 import requests
 from bs4 import BeautifulSoup as bs
-import csv
-#from lavanda_del_lago.functools import *
 from string import whitespace
 
 def white_strip(sentence:str)->str:
@@ -99,37 +97,3 @@ def product_scraper(catalogo):
             item['formato'] = 'n/a'
 
     return catalogo
-        
-def main():
-    
-    global URL 
-    URL = "https://www.lavandadellago.it/it/store/"
-
-    #use requests and bs4 to retrieve the product categories and their url query
-    store_page = requests.get(URL)
-    soup = bs(store_page.content, "lxml")
-    LdL_linee = soup.find_all(class_="btn-category")
-
-    LdL_linee = elementi(LdL_linee) #list of all product categories with dictionary {name:'',url_query:''}    
-    LdL_linee.pop(0) #remove "Tutti" category
-    
-    catalogue = (category_scraper(LdL_linee))
-    prodotti = product_scraper(catalogue)
-    ######################################
-    #         Current Position           #
-    ######################################
-
-    for item in prodotti:
-        print(item)
-    input()
-
-    #fields = ['Url Prodotto','Nome Prodotto','Linea Prodotto','Prezzo Prodotto','Codice Prodotto','Formato prodotto']  
-
-    # with open("Catalogo.csv", "w") as f:
-    #     write = csv.writer(f,delimiter = ';')
-    #     write.writerow(fields)
-    #     for row in prodotti:
-    #         write.writerow(row)
-
-if __name__ == "__main__":
-    main()
